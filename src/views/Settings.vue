8f1bac1a082ca1289be600c61272cba9cd1d4046
Tilman Justen <git@tilmanjusten.de>
2018-10-22 16:27:11 +0200

    update settings view

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="settings">
+      <h1 class="settings__title">Einstellungen</h1>
       <div class="settings__main">
-        <h1>Einstellungen</h1>
+
       </div>
     </div>
 </template>
@@ -18,10 +19,23 @@ export default {
   align-items: flex-start;
   display: flex;
   flex-wrap: wrap;
+  flex-direction: column;
+  align-items: stretch;
   min-height: 100vh;
 }
 
+.settings__title {
+  flex: 1 1 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding-top: 1rem;
+}
+
 .settings__main {
-  padding: 1rem;
+  flex: 1 1 75vh;
+  height: 75vh;
+  padding: 0 1rem;
 }
 </style>
